1f43bd02b0b92a8a40e9d411763da787a9de8a44
Kayra-Kaan <167884528+Kayra-Kaan@users.noreply.github.com>
2024-12-29 15:26:30 +0300

    Created directory_ops.h

---
+++ include/directory_ops.h
@@ -0,0 +1,9 @@
+#ifndef DIRECTORY_OPS_H
+#define DIRECTORY_OPS_H
+
+void listDirectory(const char *dir);  
+void createDirectory(const char *dir);  
+void removeDirectory(const char *dir);  
+
+#endif
+
