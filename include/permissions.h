52aac4750e13fd7ee74c47a0ebd0ddcf7477839c
Furkangnn <fgonen076@gmail.com>
2024-12-29 14:41:42 +0300

    Remove include folder from tracking

--- include/permissions.h
+++
@@ -1,8 +0,0 @@
-#ifndef PERMISSIONS_H
-#define PERMISSIONS_H
-
-// Dosya ve dizin izinleri için fonksiyon prototipleri
-void changePermissions(const char *path, mode_t mode);  // İzin değiştirme
-
-#endif
-
