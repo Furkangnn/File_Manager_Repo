af5f8ceca8ee9f194c41331aa04e5b3ee86aad3d
Furkangnn <fgonen076@gmail.com>
2024-12-29 14:28:51 +0300

    myCommit

---
+++ include/logger.h
@@ -0,0 +1,8 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+// Log yazma işlemleri için fonksiyon prototipleri
+void logOperation(const char *operation, const char *result);  // İşlem kaydı
+
+#endif
+
---
+++ include/permissions.h
@@ -0,0 +1,8 @@
+#ifndef PERMISSIONS_H
+#define PERMISSIONS_H
+
+// Dosya ve dizin izinleri için fonksiyon prototipleri
+void changePermissions(const char *path, mode_t mode);  // İzin değiştirme
+
+#endif
+
